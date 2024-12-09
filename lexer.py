0536fe0eba6c1685d718702fd88dbd9e344d286e
edrill <edrillb@gmail.com>
2024-12-09 17:26:27 +0800

    Added str delim = }

--- lexer.py
+++ lexer.py
@@ -50,7 +50,7 @@ delim_map = {
     'plus_delim':       set(ALPHA_NUMERIC + '"' + "'" + '-' + '(' + ',' + ' '),
     'para_delim':       {'(', ' ', '\n'},
     'recall_delim':     set(ALPHA + ' ' + ';'), 
-    'str_delim':        {'+', ')', ']', '\n', ',', ';', ' ', ':'},
+    'str_delim':        {'+', ')', ']', '\n', ',', ';', ' ', ':', '}'},
     'white_delim':      set(ASCII + ALL_OPERATOR + ' ' + '\n' + '\t' + '\0'),
     'woogie_delim':     set(NUMERIC + '(' + ' ')
 }
