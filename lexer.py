1c7495d1f9225d19a98aaba73c86e31fa3aa01fb
edrill <edrillb@gmail.com>
2024-12-10 01:06:10 +0800

    Now tokenizes space, tab, and newline

--- lexer.py
+++ lexer.py
@@ -195,6 +195,10 @@ TT_COL      = 'COLON'   # ':'
 TT_COMMA    = 'COMMA'   # ','
 
 TT_EOF      = 'EOF'     # End of File
+TT_SPACE    = 'SPACE'   # Space ' '
+TT_TAB      = 'TAB'     # Newline '\n'
+TT_NEWLINE  = 'NEWLINE' # Tab '\t'
+
 
 TT_KEYWORD  = 'KEYWORD' # Keywords
 TT_IDENTIFIER = 'IDENTIFIER' # Identifiers
@@ -235,21 +239,8 @@ class Lexer:
         tokens = []
 
         while self.current_char is not None:
-
-
-            if self.current_char in ' \t\n':
-                self.advance()
-                if self.current_char not in delim_map['white_delim']:
-                    return tokens, LexicalError(pos_start, self.pos, f"Invalid delimiter '{self.current_char}' after space, newline, or tab'")
-
-
-            elif self.current_char in NUMERIC:
-                tok, error = self.make_number()     # function for making integer and float tokens
-                if error: return tokens, error
-                tokens.append(tok)
-
             
-            elif self.current_char in ALPHA:
+            if self.current_char in ALPHA:
                 ident_str = ''
                 ident_count = 0
                 pos_start = self.pos.copy()
@@ -1005,13 +996,7 @@ class Lexer:
                 tokens.append(Token(TT_IDENTIFIER, ident_str, pos_start=pos_start, pos_end=self.pos)) 
 
 
-            elif self.current_char == '"':
-                tok, error = self.make_string()
-                if error: return tokens, error
-                tokens.append(tok)
-
-
-            elif self.current_char == '=':
+            elif self.current_char == '=':      # assignment operator, equals 
                 tok_type = TT_ASSIGN
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1029,7 +1014,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '==', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '+':
+            elif self.current_char == '+':          # plus, increment, plus equals
                 tok_type = TT_PLUS
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1054,7 +1039,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '+=', pos_start=pos_start, pos_end=self.pos))
                 
 
-            elif self.current_char == '-': 
+            elif self.current_char == '-':          # minus, decrement, minus equals
                 pos_start = self.pos.copy()
                 self.advance()
 
@@ -1084,7 +1069,7 @@ class Lexer:
                             return tokens, LexicalError(pos_start, self.pos, f"Unexpected '-' without a valid number or identifier.")
 
         
-            elif self.current_char == '*':
+            elif self.current_char == '*':      # multiply, power operator, multiply equals,
                 tok_type = TT_MUL
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1109,7 +1094,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '*=', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '/':
+            elif self.current_char == '/':      # divide, divide equals
                 tok_type = TT_DIV
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1127,7 +1112,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '/=', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '%':
+            elif self.current_char == '%':      # modulo, modulo equals
                 tok_type = TT_MOD
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1145,7 +1130,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '%=', pos_start=pos_start, pos_end=self.pos))
             
 
-            elif self.current_char == '!':
+            elif self.current_char == '!':      # not, not equals
                 tok_type = TT_NOT
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1163,7 +1148,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '!=', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '<':
+            elif self.current_char == '<':      # less than, less than or equal
                 tok_type = TT_LT
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1181,7 +1166,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '<=', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '>':
+            elif self.current_char == '>':          # greater than, greater than or equal
                 tok_type = TT_GT
                 pos_start = self.pos.copy()
                 self.advance()
@@ -1199,7 +1184,7 @@ class Lexer:
                     tokens.append(Token(tok_type, '>=', pos_start=pos_start, pos_end=self.pos))
 
 
-            elif self.current_char == '&':
+            elif self.current_char == '&':          # and operator
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char == '&':
@@ -1210,7 +1195,7 @@ class Lexer:
                 else: return tokens, InvalidSyntaxError(pos_start, self.pos, "'&' is not a valid operator")
 
 
-            elif self.current_char == '|':
+            elif self.current_char == '|':          # or operator
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char == '|':
@@ -1221,7 +1206,7 @@ class Lexer:
                 else: return tokens, InvalidSyntaxError(pos_start, self.pos, "'|' is not a valid operator")
     
 
-            elif self.current_char == '(':
+            elif self.current_char == '(':          # left parenthesis
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['opnparen_delim']:
@@ -1229,7 +1214,7 @@ class Lexer:
                 tokens.append(Token(TT_LPAREN, '(', pos_start=self.pos))
 
 
-            elif self.current_char == ')':
+            elif self.current_char == ')':          # right parenthesis
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['clsparen_delim']:
@@ -1237,7 +1222,7 @@ class Lexer:
                 tokens.append(Token(TT_RPAREN, ')', pos_start=self.pos))
 
 
-            elif self.current_char == '[':
+            elif self.current_char == '[':          # left bracket
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['opnsquare_delim']:
@@ -1245,7 +1230,7 @@ class Lexer:
                 tokens.append(Token(TT_LSQUARE, '[', pos_start=self.pos))
 
 
-            elif self.current_char == ']':
+            elif self.current_char == ']':          # right bracket
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['clssquare_delim']:
@@ -1253,7 +1238,7 @@ class Lexer:
                 tokens.append(Token(TT_RSQUARE, ']', pos_start=self.pos))
 
 
-            elif self.current_char == '{':
+            elif self.current_char == '{':          # left brace
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['opnbrace_delim']:
@@ -1261,7 +1246,7 @@ class Lexer:
                 tokens.append(Token(TT_LBRACE, '{', pos_start=self.pos))
 
 
-            elif self.current_char == '}':
+            elif self.current_char == '}':          # right brace
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char != None and self.current_char not in delim_map['clsbrace_delim']:
@@ -1269,7 +1254,7 @@ class Lexer:
                 tokens.append(Token(TT_RBRACE, '}', pos_start=self.pos))
 
 
-            elif self.current_char == ',':
+            elif self.current_char == ',':          # comma
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['comma_delim']:
@@ -1277,7 +1262,7 @@ class Lexer:
                 tokens.append(Token(TT_COMMA, ',', pos_start=self.pos))
 
 
-            elif self.current_char == ':':
+            elif self.current_char == ':':          # colon
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char not in delim_map['col_delim']:
@@ -1285,7 +1270,7 @@ class Lexer:
                 tokens.append(Token(TT_COL, ':', pos_start=self.pos))
 
 
-            elif self.current_char == ';':
+            elif self.current_char == ';':          # semicolon
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char != None and self.current_char not in delim_map['lend_delim']:
@@ -1293,7 +1278,21 @@ class Lexer:
                 tokens.append(Token(TT_SEMICOL, ';', pos_start=self.pos))
 
 
-            elif self.current_char == '#':
+
+
+            elif self.current_char in NUMERIC:
+                tok, error = self.make_number()     # function for making integer and float tokens
+                if error: return tokens, error
+                tokens.append(tok)
+
+
+            elif self.current_char == '"':          # function for making string
+                tok, error = self.make_string()
+                if error: return tokens, error      
+                tokens.append(tok)
+
+
+            elif self.current_char == '#':          # ignore single and multi-line comments
                 pos_start = self.pos.copy()
                 self.advance()
                 if self.current_char == '$':
@@ -1306,6 +1305,38 @@ class Lexer:
                                 break
                 while self.current_char != None and self.current_char in ASCII + ' \t':
                     self.advance()
+            
+
+            if self.current_char in ' \t\n':        # whitespace, newline, tab
+                pos_start = self.pos
+
+                # check for space
+                if self.current_char == ' ':
+                    while self.current_char == ' ':
+                        self.advance()
+                    if self.current_char not in delim_map['white_delim']:
+                        return tokens, LexicalError(pos_start, self.pos, f"Invalid delimiter '{self.current_char}' after space")
+                    else:
+                        tokens.append(Token(TT_SPACE, "' '", pos_start=pos_start, pos_end=self.pos))
+                        continue
+                # check for tab
+                if self.current_char == '\t':
+                    while self.current_char == '\t':
+                        self.advance()
+                    if self.current_char not in delim_map['white_delim']:
+                        return tokens, LexicalError(pos_start, self.pos, f"Invalid delimiter '{self.current_char}' after tab")
+                    else:
+                        tokens.append(Token(TT_TAB, '\\t', pos_start=pos_start, pos_end=self.pos))
+                        continue
+                # check for newline
+                if self.current_char == '\n':
+                    while self.current_char == '\n':
+                        self.advance()
+                    if self.current_char not in delim_map['white_delim']:
+                        return tokens, LexicalError(pos_start, self.pos, f"Invalid delimiter '{self.current_char}' after newline")
+                    else:
+                        tokens.append(Token(TT_NEWLINE, '\\n', pos_start=pos_start, pos_end=self.pos))
+                        continue
 
             else: 
                 pos_start = self.pos.copy()
@@ -1319,7 +1350,8 @@ class Lexer:
 
     def make_number(self, is_negative=False):  # for making numbers: int and float
         num_str = ''
-        num_count = 0
+        int_count = 0
+        num_count = 0 
         dec_count = 0
         dot_count = 0
         pos_start = self.pos.copy()
@@ -1335,19 +1367,33 @@ class Lexer:
                     return [], LexicalError(pos_start, pos_end, f"Multiple period '.' in a float assignment")
                 elif self.current_char == None or self.current_char not in NUMERIC:
                     pos_end = self.pos.copy()  
-                    return [], LexicalError(pos_start, pos_end, "Incomplete float assignment")
+                    return [], LexicalError(pos_start, pos_end, "Invalid float assignment")
                 dot_count += 1
                 num_str += '.'
             else:
+                # checks dot count to see if num is int or float,
+                # if dot count is 0, num is int, increment num_count and int_count
+                # if dot count is 1, num is float and were in the least significat, increment dec_count
                 if dot_count == 0:
                     num_count += 1
+                    int_count +=1
                 if dot_count == 1:
                     dec_count += 1
-                if num_count > 9:
+
+                # checks if num_count exceeds limit of 17 if number is an int
+                if dot_count == 0 and num_count > 17:
                     pos_end = self.pos.copy()
                     return [], LexicalError(pos_start, pos_end, "Whole number exceeded maximum character limit of 17")
+                
+                # checks if num_count exceeds limit of 9 if number is a float
+                if dot_count == 1 and num_count > 9:
+                    pos_end = self.pos.copy()
+                    return [], LexicalError(pos_start, pos_end, "Whole number exceeded maximum character limit of 17")
+                
+                # checks if dec_count exceeds limit of 7 if number is a float, if it exceeds then just ignore
                 if dot_count == 1 and dec_count > 7:
                     self.advance()
+                # append the latest character to the number string
                 else:
                     num_str += self.current_char
                     self.advance()
@@ -1356,6 +1402,9 @@ class Lexer:
             pos_end = self.pos.copy()
             return [], LexicalError(pos_start, pos_end, f"Invalid delimiter '{self.current_char}' after number")
 
+        if dot_count > 1:
+            pos_end = self.pos.copy()
+            return [], LexicalError(pos_start, pos_end, f"Multiple period '.' in a float assignment")
         if dot_count == 0:
             return Token(TT_INT, int(num_str), pos_start, self.pos), None
         else:
@@ -1366,7 +1415,7 @@ class Lexer:
         pos_start = self.pos.copy()
         self.advance()
 
-        while self.current_char != None and self.current_char in ASCII + ' ':
+        while self.current_char != None and self.current_char in ASCII:
             if self.current_char == '"':
                 self.advance() 
                 if self.current_char not in delim_map['str_delim']:
@@ -1377,7 +1426,7 @@ class Lexer:
 
         return [], InvalidSyntaxError(pos_start, self.pos, 'String not properly closed with double quotes (")')
 
-    
+
 def run(fn, text):
         lexer = Lexer(fn, text)
         tokens, error = lexer.make_tokens()
--- shell.py
+++ shell.py
@@ -1,48 +1,45 @@
 import customtkinter as ctk
 import lexer
 
-# Appearance settings
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
-# List of keywords
 keywords = [
     "domain", "expansion", "null", "int", "float", "string", "bool", 
     "restrict", "invoke", "capture", "true", "false", 
     "vow", "else vow", "else", "boogie", "woogie", 
     "default", "cycle", "sustain", "perform", 
     "dismiss", "hop", "recall", "cleave", 
-    "dismantle", "len()", "curse"
+    "dismantle", "len", "curse"
 ]
 
-# Function to process the input (color highlighting only)
-def process_input_color(event=None):
-    text = input_text.get("1.0", "end").strip()  # Get input text
+def process_input_color(event=None):                # function to process the input color
+    text = input_text.get("1.0", "end").strip()     # Get input text
     if not text:
         return
 
-    # Clear previous highlights
+    # clear previous highlights
     input_text.tag_remove("purple", "1.0", "end")
     input_text.tag_remove("comment", "1.0", "end")
-    input_text.tag_remove("string", "1.0", "end")  # Clear string highlights
+    input_text.tag_remove("string", "1.0", "end")  # clear string highlights
 
-    # Highlight keywords from the list
+    # highlight keywords from the list
     for keyword in keywords:
         start_index = "1.0"
         while True:
-            # Find the next occurrence of the keyword
+            # find the next occurrence of the keyword
             start_index = input_text.search(keyword, start_index, stopindex="end")
             if not start_index:
                 break
             end_index = f"{start_index}+{len(keyword)}c"
-            # Apply the tag to the keyword
-            input_text.tag_add("purple", start_index, end_index)
+            # apply the tag to the keyword
+            input_text.tag_add("keyword", start_index, end_index)
             start_index = end_index  # Move to the next character after the keyword
 
-    # Define the tag for magenta color (keyword highlight)
-    input_text.tag_config("purple", foreground="#f396d3")
+    # define the tag for magenta color (keyword highlight)
+    input_text.tag_config("keyword", foreground="#f396d3")
 
-    # Highlight single-line comments starting with '#'
+    # highlight single-line comments starting with '#'
     start_index = "1.0"
     while True:
         start_index = input_text.search("#", start_index, stopindex="end")
@@ -52,7 +49,7 @@ def process_input_color(event=None):
         input_text.tag_add("comment", start_index, line_end)
         start_index = line_end
 
-    # Highlight multi-line comments starting with '#$' and ending with '$#'
+    # highlight multi-line comments starting with '#$' and ending with '$#'
     start_index = "1.0"
     while True:
         start_index = input_text.search("#\\$", start_index, stopindex="end", regexp=True)
@@ -66,35 +63,35 @@ def process_input_color(event=None):
         input_text.tag_add("comment", start_index, end_index)
         start_index = end_index
 
-    # Define the tag for green color (comment highlight)
+    # hefine the tag for gray color (comment highlight)
     input_text.tag_config("comment", foreground="#999999")
 
-    # Highlight strings, including multi-line strings
+    # highlight strings, including multi-line strings
     start_index = "1.0"
     while True:
-        # Find the starting double quote for strings
+        # find the starting double quote for strings
         start_index = input_text.search(r'"', start_index, stopindex="end", regexp=True)
         if not start_index:
             break
 
-        # Check if there is a closing quote on the same line
+        # check if there is a closing quote on the same line
         end_index = input_text.search(r'"', f"{start_index}+1c", stopindex="end", regexp=True)
         
         if not end_index:
-            # If there's no closing quote on the same line, find it in subsequent lines
-            # Search for a quote in the next lines
+            # if there's no closing quote on the same line, find it in subsequent lines
+            # search for a quote in the next lines
             end_index = input_text.search(r'"', f"{start_index}+1l", stopindex="end", regexp=True)
         
         if not end_index:
-            end_index = input_text.index("end")  # If no closing quote, highlight until the end
+            end_index = input_text.index("end")  # if no closing quote, highlight until the end
         else:
-            end_index = f"{end_index}+1c"  # Include the closing double quote in the highlight
+            end_index = f"{end_index}+1c"  # include the closing double quote in the highlight
 
-        # Apply the tag to the string
+        # apply the tag
         input_text.tag_add("string", start_index, end_index)
-        start_index = end_index  # Move past the highlighted string
+        start_index = end_index  # move past the highlighted string
 
-    # Define the tag for orange color (string highlight)
+    # tag color configuration
     input_text.tag_config("string", foreground="#FFCA4B")
 
 def process_input(event=None):
@@ -102,82 +99,82 @@ def process_input(event=None):
     if not text:
         return
 
-    # Run lexer
+    # retrieve tokens, error from lexer 
     tokens, error = lexer.run('<stdin>', text)
 
-    # Update tokens output (token types)
-    tokens_output.configure(state="normal")  # Enable editing temporarily
-    tokens_output.delete("1.0", "end")  # Clear previous tokens
+    # update tokens output (token types)
+    tokens_output.configure(state="normal")  # enable editing temporarily
+    tokens_output.delete("1.0", "end")  # clear previous tokens
     if tokens:
         token_types = [f"{i + 1}:  {token.type}" for i, token in enumerate(tokens)]  # Add line numbers
         tokens_output.insert("1.0", '\n'.join(token_types))
-    tokens_output.configure(state="disabled")  # Disable editing after update
+    tokens_output.configure(state="disabled")  # disable editing after update
 
-    # Update lexeme output
-    lexeme_output.configure(state="normal")  # Enable editing temporarily
-    lexeme_output.delete("1.0", "end")  # Clear previous lexemes
+    # update lexeme output
+    lexeme_output.configure(state="normal")  # enable editing temporarily
+    lexeme_output.delete("1.0", "end")  # clear previous lexemes
     if tokens:
         lexemes = [f"{i + 1}:  {token.value}" for i, token in enumerate(tokens)]  # Add line numbers
         lexeme_output.insert("1.0", '\n'.join(lexemes))
-    lexeme_output.configure(state="disabled")  # Disable editing after update
+    lexeme_output.configure(state="disabled")  # disable editing after update
 
-    # Update error output
-    error_output.configure(state="normal")  # Enable editing temporarily
-    error_output.delete("1.0", "end")  # Clear previous errors
+    # update error output
+    error_output.configure(state="normal")  # enable editing temporarily
+    error_output.delete("1.0", "end")  # clear previous errors
     if error:
         error_output.insert("1.0", error.as_string())
-    error_output.configure(state="disabled")  # Disable editing after update
+    error_output.configure(state="disabled")  # disable editing after update
 
 
 app = ctk.CTk()
 app.title("DOM Lexer")
-app.geometry("840x545")  # Adjust width to fit the tokens area comfortably
+app.geometry("1040x680")  # window resolution
 app.resizable(False, False)
 
 try:
-    app.iconbitmap("dom_logo.ico")  # Replace with your .ico file path
+    app.iconbitmap("dom_logo.ico") 
 except Exception as e:
     print(f"Could not set icon: {e}")
 
-# Input section
+# input section
 input_label = ctk.CTkLabel(app, text="Input Code:", font=("Arial", 16))
 input_label.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
-input_text = ctk.CTkTextbox(app, width=500, height=280, font=("Verdana", 14), wrap="none")
+input_text = ctk.CTkTextbox(app, width=700, height=400, font=("Verdana", 14), wrap="none")
 input_text.grid(row=1, column=0, padx=20, pady=0, sticky="n")
 
 input_text.configure(state="normal") 
 input_text.bind("<KeyRelease>", process_input_color)
 
-# Create Tokenize Input button to process tokens and errors
-process_button = ctk.CTkButton(app, text="Tokenize Input", command=process_input, width=390)
+# tokenize Button
+process_button = ctk.CTkButton(app, text="Tokenize Input", command=process_input, width=590)
 process_button.grid(row=2, column=0, padx=20, pady=10, sticky="n")
 
-# Error section (below input text)
+# error section
 error_label = ctk.CTkLabel(app, text="Errors:", font=("Arial", 16))
 error_label.grid(row=3, column=0, padx=20, pady=5, sticky="n")
 
-# Error textbox without scrollbars and no wrapping
-error_output = ctk.CTkTextbox(app, width=500, height=119, font=("Consolas", 14), state="disabled", wrap="none")
+# error textbox
+error_output = ctk.CTkTextbox(app, width=700, height=130, font=("Consolas", 14), state="disabled", wrap="none")
 error_output.grid(row=4, column=0, padx=20, pady=0, sticky="s")
 
-# Tokens section (right-side rectangle)
+# tokens section 
 tokens_label = ctk.CTkLabel(app, text="Token Type:", font=("Arial", 16))
 tokens_label.grid(row=0, column=1, padx=0, pady=5, sticky="ew")
 
-tokens_output = ctk.CTkTextbox(app, width=170, height=485, font=("Verdana", 14), state="disabled", wrap="none")
+tokens_output = ctk.CTkTextbox(app, width=170, height=620, font=("Verdana", 14), state="disabled", wrap="none")
 tokens_output.grid(row=1, rowspan=5, column=1, padx=20, pady=0, sticky="n")
 
-# Tokens section (right-side rectangle)
+# tokens section 
 lexeme_label = ctk.CTkLabel(app, text="Lexemes:", font=("Arial", 16))
 lexeme_label.grid(row=0, column=2, padx=20, pady=5, sticky="ew")
 
-lexeme_output = ctk.CTkTextbox(app, width=170, height=485, font=("Verdana", 14), state="disabled", wrap="none")  
+lexeme_output = ctk.CTkTextbox(app, width=170, height=620, font=("Verdana", 14), state="disabled", wrap="none")  
 lexeme_output.grid(row=1, rowspan=5, column=2, padx=20, pady=0, sticky="n")
 
-app.grid_columnconfigure(0, weight=1)  # Input column
-app.grid_columnconfigure(1, weight=0)  # Tokens column
-app.grid_columnconfigure(2, weight=0)  # Lexeme column
+app.grid_columnconfigure(0, weight=1)  # input column
+app.grid_columnconfigure(1, weight=0)  # tokens column
+app.grid_columnconfigure(2, weight=0)  # lexeme column
 app.grid_rowconfigure(1, weight=0) 
 
 app.mainloop()
