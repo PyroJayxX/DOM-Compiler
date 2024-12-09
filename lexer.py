6156ab5a7668cab66f3fdc6be47f78063374d3e2
edrill <edrillb@gmail.com>
2024-12-10 02:02:56 +0800

    Minor bug fixes

--- lexer.py
+++ lexer.py
@@ -55,28 +55,6 @@ delim_map = {
     'woogie_delim':     set(NUMERIC + '(' + ' ')
 }
 
-first_set_map = {
-    'fs_program':           {'expansion', '#', '#$'},
-    'fs_global_dec':        {'int', 'string', 'float', 'bool', 'curse', 'restrict', '#', '#$', None}, # + id
-    'fs_body':              {'int', 'string', 'float', 'bool', 'curse', 'restrict', 'invoke', 'cycle', 'vow', 'while', '#', '#$', None}, # +id
-    'fs_local_dec':         {'int', 'string', 'float', 'bool', 'curse', 'restrict', '#', '#$', None}, # + id
-    'fs_var_dec':           {'int', 'string', 'float', 'bool', 'restrict'},
-    'fs_clan_dec':          {'int', 'string', 'float'},
-    'fs_curse_dec':         {'curse'},
-    'fs_invoke_stm':        {'invoke'},
-    'fs_capture_stm':       {'capture'},
-    'fs_conditional_stm':   {'vow'},
-    'fs_boogie_woogie_stm': {'boogie'},
-    'fs_cycle_loop':        {'cycle'},
-    'fs_sustain_loop':      {'sustain'},
-    'fs_persustain_loop':   {'perform'},
-    'fs_unary_expr':        {'++', '--', '!'},
-    'fs_arith_expr':        {'('},
-    'fs_relational_expr':   {'len', '('},
-    'fs_length':            {'len'},
-    'fs_cleave':            {'cleave'},
-    'fs_dismantle':         {'dismantle'}
-    }
 
 ##############
 # ERRORS
@@ -101,29 +79,6 @@ class LexicalError(Error):
 class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details=''):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
-
-class RTError(Error):
-    def __init__(self, pos_start, pos_end, details, context):
-        super().__init__(pos_start, pos_end, 'Runtime Error', details)
-        self.context = context
-
-    def as_string(self):
-        result = self.generate_traceback()
-        result += f'{self.error_name}: {self.details}'
-        result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-        return result
-    
-    def generate_traceback(self):
-        result = ''
-        pos = self.pos_start
-        ctx = self.context
-
-        while ctx:
-            result = f' File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
-            pos = ctx.parent_entry_pos
-            ctx = ctx.parent
-        
-        return 'Traceback (most recent call last):\n' + result
 
 ##############
 # POSITION
